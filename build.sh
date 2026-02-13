1aa8ad6fbae96acd64f9c27788db46eb771e91e9
davidevigolo <davide.vigolo.04@gmail.com>
2026-02-13 21:47:35 +0100

    removed build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-cd DipReader
-docker run --rm \
- -v $(pwd):/project \
- -v ~/.cache/electron:/root/.cache/electron \
- -v ~/.cache/electron-builder:/root/.cache/electron-builder \
- electronuserland/builder:wine \
- /bin/bash -c "rm -rf node_modules && npm install --platform=win32 --arch=x64 && npm run dist -- --win --x64"
